280231ef4ef9963bb118607debad52de7def063f
Irina Bright <missirinabright@gmail.com>
2019-05-17 01:31:12 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -4,9 +4,10 @@ def levitation_quiz
 	loop do 
   	puts "What is the spell that enacts levitation?
         break if answer == "Wingardium Leviosa"
+      end
     puts "You passed the quiz!"
 end
-end
+
 
 
 
